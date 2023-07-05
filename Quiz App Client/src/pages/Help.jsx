e50bb574c504830f9b2723da0681ae364b35b674
abid2330 <138620429+abid2330@users.noreply.github.com>
2023-07-05 08:36:03 +0500

    Add files via upload
    first

---
+++ Quiz App Client/src/pages/Help.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Help = () => {
+    return (
+        <>
+            <div style={{ position: "absolute", left: "0", right: "0", bottom: "0", top: "0px", margin: "0", padding: "0", height: "100%", overflow: "hidden" }}>
+                <iframe src="https://drive.google.com/file/d/15pJNZzfbzEoM2rwGQC_kMqJce8Tz6NVu/view?usp=drive_link" frameBorder="0" allow="autoplay; encrypted-media" allowFullScreen title="help" width="100%" height="90%"></iframe>
+                <p>also here is a link for the help video :<a href="https://drive.google.com/file/d/15pJNZzfbzEoM2rwGQC_kMqJce8Tz6NVu/view?usp=drive_link">click here</a></p>
+
+            </div>
+
+        </>
+    )
+}
+
+export default Help
